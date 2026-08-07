c072dc0e5ab836e6576945fc9f57b8e2e14e3d2d
julio jijon <jijonj@berea.edu>
2026-08-07 10:20:29 -0700

    introduced change that fixes issue #91 (#128)
    * introduced change that fixes issue 91
    
    * changed based on feedback
    
    ---------
    
    Co-authored-by: Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>

--- src/main.ts
+++ src/main.ts
@@ -96,6 +96,7 @@ async function getMetaData(
   existingCodeJSON?: CodeJSON | null,
 ): Promise<Partial<CodeJSON>> {
   const partialCodeJSON = await helpers.calculateMetaData();
+  const version = existingCodeJSON?.version || partialCodeJSON.version;
 
   // preserve existing feedback mechanisms if they exist, otherwise default to GitHub Issues
   const feedbackMechanism =
@@ -158,6 +159,7 @@ async function getMetaData(
 
   return {
     name: partialCodeJSON.name,
+    version: version,
     description: description,
     status: status ?? blankEnumValue,
     repositoryURL: partialCodeJSON.repositoryURL,
